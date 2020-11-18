762397c052f5a940536bf29feef09e494776ea4b
markconigliaro1 <markconigliaro1@gmail.com>
2020-11-18 09:12:34 -0500

    Flips condition, aborting with errorif token isn't available

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,9 +5,11 @@ set -e
 # Set a default JAVA_TOOL_OPTIONS if it hasn't already been specified in .travis.yml
 export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:--Xmx4096m}"
 
-if [[ -n "${SONAR_TOKEN:-}" ]]; then
-  echo "======================================"
-  echo "Running SonarQube Analysis"
-  echo "======================================"
-  ./gradlew sonarqube -i
-fi
+if [[ -z "${SONAR_TOKEN:-}" ]]; then
+  echo "SonarCloud token not present in the environment. Aborting."
+  exit 1
+fi  
+echo "======================================"
+echo "Running SonarQube Analysis"
+echo "======================================"
+./gradlew sonarqube -i
