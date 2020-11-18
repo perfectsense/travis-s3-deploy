3772c491777adf7c5fb891fbbb67634c6dba7501
markconigliaro1 <markconigliaro1@gmail.com>
2020-11-18 13:58:04 -0500

    Updates with project version

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,11 +5,13 @@ set -e
 # Set a default JAVA_TOOL_OPTIONS if it hasn't already been specified in .travis.yml
 export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:--Xmx4096m}"
 
-if [[ -z "${SONAR_TOKEN:-}" ]]; then
-  echo "SonarCloud token not present in the environment. Aborting."
-  exit 1
-fi  
-echo "======================================"
-echo "Running SonarQube Analysis"
-echo "======================================"
-./gradlew sonarqube -i
+if [[ -n "${SONAR_TOKEN:-}" ]]; then
+  version=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD || echo "0")
+  version=${version/v/}
+  
+  echo "======================================"
+  echo "Running SonarQube Analysis for version ${version}"
+  echo "======================================"
+
+  ./gradlew sonarqube -i -PsonarProjectVersion="${version}"
+fi
