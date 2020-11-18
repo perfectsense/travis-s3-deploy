34447b89b8bcbeb29b9f69aafeb0b2ed3653fd0e
markconigliaro1 <markconigliaro1@gmail.com>
2020-11-18 14:01:23 -0500

    Flips condition... again

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,13 +5,16 @@ set -e
 # Set a default JAVA_TOOL_OPTIONS if it hasn't already been specified in .travis.yml
 export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:--Xmx4096m}"
 
-if [[ -n "${SONAR_TOKEN:-}" ]]; then
-  version=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD || echo "0")
-  version=${version/v/}
-  
-  echo "======================================"
-  echo "Running SonarQube Analysis for version ${version}"
-  echo "======================================"
-
-  ./gradlew sonarqube -i -PsonarProjectVersion="${version}"
+if [[ -z "${SONAR_TOKEN:-}" ]]; then
+    echo "SonarCloud token not present in the environment. Aborting."
+    exit 1
 fi
+
+version=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD || echo "0")
+version=${version/v/}
+
+echo "======================================"
+echo "Running SonarQube Analysis for version ${version}"
+echo "======================================"
+
+./gradlew sonarqube -i -PsonarProjectVersion="${version}"
